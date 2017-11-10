2a2335ba914701a10bcefe54d9f1803aeca216e0
abp491 <fabian.reiber@haw-hamburg.de>
2017-11-10 12:04:00 +0100

    modified emoji plotting

--- tutorial/processor/emoji_chart_processor.py
+++ tutorial/processor/emoji_chart_processor.py
@@ -17,27 +17,37 @@ class EmojiChartProcessor(BaseProcessor):
     def process(self, key, value):
         self._create_bar_chart(value)
 
+    def punctuate(self, timestamp):
+        pass
+
     def _create_bar_chart(self, emoji):
         self.emojis.setdefault(emoji, 0)
         self.emojis[emoji] += 1
         # sort emojis and return the most favored
         emojis_sorted = sorted(self.emojis.items(),
                                key=lambda e: e[1],
-                               reverse=True)[:20]
-        emoji_list = [k[0].decode('utf-8') for k in emojis_sorted]
+                               reverse=True)[:10]
+        # append empty char for beautiful emoji plotting :)
+        emoji_list = ['{} '.format(k[0].decode('utf-8'))
+                      for k in emojis_sorted]
         count_list = [v[1] for v in emojis_sorted]
         data = {'emoji_counts': pd.Series(count_list, index=emoji_list)}
         df = pd.DataFrame(data)
-        _, axis = plt.subplots()
+        fig, axis = plt.subplots()
+        # set font of x-axis
+        for label in (axis.get_xticklabels()):
+            label.set_fontname('EmojiOne Color')
+            label.set_fontsize(10)
         axis.set_xlabel('emoji')
         axis.set_ylabel('total counts')
         df['emoji_counts'].plot(
             kind='bar',
             ax=axis,
-            title='20th most favored emojis :)\n'
+            title='10th most favored emojis :)\n'
             '{} emojis totally counted'.format(len(self.emojis)),
             legend=True)
         buf = BytesIO()
+        plt.xticks(rotation=0)
         plt.savefig(buf, dpi=300, format='png')
         self.context.forward('chart', buf.getvalue())
         buf.close()
--- tutorial/tests/run.sh
+++ tutorial/tests/run.sh
@@ -2,10 +2,10 @@
 
 chart_path="chart.png"
 if [[ $1 ]]; then
-	x-terminal-emulator -e python producer/twitter_producer.py -t twitter -s '#halloween' 
+	x-terminal-emulator -e python producer/twitter_producer.py -t twitter -s 'trump' 
 	x-terminal-emulator -e python processor/twitter_pipeline.py -st twitter -tt emoji-chart
 	x-terminal-emulator -e python consumer/chart_consumer.py -t emoji-chart -f ${1}${chart_path}
 else
 	echo "$BASH_SOURCE <relative-path-to-consumer-image-loader>"
-	echo "i.g.: $BASH_SOURCE consumer/image-loader/${chart_name}"	
+	echo "i.g.: $BASH_SOURCE consumer/image-loader/"	
 fi	
